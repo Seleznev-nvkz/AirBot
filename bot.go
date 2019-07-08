166f65c1654113432c9c022a676dd637e7b790ed
Viktor Savelev <tugotron45@gmail.com>
2019-07-08 21:34:43 +0300

    Implemented image send;

--- bot.go
+++ bot.go
@@ -17,7 +17,7 @@ func handleCommand(message *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(message.Chat.ID, "")
 
 	switch message.Command() {
-	case "subscribe":
+	case "subscribe", "start":
 		config.appendSubscriber(message.Chat.ID)
 		log.Printf("Subsribed %v", message.Chat.ID)
 		msg.Text = "You successful subscribed."
@@ -31,7 +31,7 @@ func handleCommand(message *tgbotapi.Message) {
 		msg.Text = sensor.FreshString()
 	case "graph":
 		buildGraph()
-		absPath, _ := filepath.Abs("./plot.png")
+		absPath, _ := filepath.Abs("plot.png")
 		sendPhoto(absPath)
 		return
 	default:
@@ -64,7 +64,7 @@ func sendMsg(msg string) {
 func sendPhoto(fileId string) {
 	for _, id := range config.Subscribers {
 		log.Println(id)
-		if _, err := bot.Send(tgbotapi.NewPhotoShare(id, fileId)); err != nil {
+		if _, err := bot.Send(tgbotapi.NewPhotoUpload(id, fileId)); err != nil {
 			log.Panic(err)
 		}
 	}

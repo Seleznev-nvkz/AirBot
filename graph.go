69d268c5e2ccd418e71c10ec001d00c2119a08b7
Viktor Savelev <tugotron45@gmail.com>
2019-07-11 15:06:09 +0300

    Show humidity; Fixed styles;

--- graph.go
+++ graph.go
@@ -12,33 +12,45 @@ func buildGraph() {
 	var (
 		temp      []float64
 		co2       []float64
+		humidity  []float64
 		timestamp []time.Time
 	)
 
 	for _, v := range GetRecentStats() {
 		temp = append(temp, v.Sensor.Temp)
 		co2 = append(co2, float64(v.Sensor.CO2/10))
+		humidity = append(humidity, v.Sensor.Humidity)
 		timestamp = append(timestamp, v.Timestamp)
 	}
 
-	ts1 := chart.TimeSeries{
+	temperatureTs := chart.TimeSeries{
 		Style: chart.Style{
 			Show:      true,
-			FillColor: chart.ColorBlue.WithAlpha(70),
+			FillColor: chart.ColorBlue.WithAlpha(15),
 		},
 		Name:    "Temperature",
 		XValues: timestamp,
 		YValues: temp,
 	}
 
-	ts2 := chart.TimeSeries{
+	co2Ts := chart.TimeSeries{
 		Style: chart.Style{
 			Show:      true,
-			FillColor: chart.ColorGreen.WithAlpha(50),
+			FillColor: chart.ColorGreen.WithAlpha(15),
 		},
 		Name:    "CO2 x10",
 		XValues: timestamp,
 		YValues: co2,
+	}
+
+	humidityTs := chart.TimeSeries{
+		Style: chart.Style{
+			Show:      true,
+			FillColor: chart.ColorBlack.WithAlpha(15),
+		},
+		Name:    "Humidity",
+		XValues: timestamp,
+		YValues: humidity,
 	}
 
 	graph := chart.Chart{
@@ -52,8 +64,9 @@ func buildGraph() {
 			Style:     chart.StyleShow(),
 		},
 		Series: []chart.Series{
-			ts1,
-			ts2,
+			temperatureTs,
+			co2Ts,
+			humidityTs,
 		},
 	}
 

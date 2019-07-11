967b4ef88ad337a3021584d723656884a3c4586e
Viktor Savelev <tugotron45@gmail.com>
2019-07-11 14:18:01 +0300

    Building graphs using go-chart lib;

--- graph.go
+++ graph.go
@@ -1,45 +1,82 @@
 package main
 
 import (
-	"github.com/Arafatk/glot"
+	"bytes"
+	"github.com/wcharczuk/go-chart"
+	"log"
+	"os"
 	"time"
 )
 
 func buildGraph() {
 	var (
-		temp []float64
-		co2  []float64
+		temp      []float64
+		co2       []float64
+		timestamp []time.Time
 	)
 
 	for _, v := range GetRecentStats() {
 		temp = append(temp, v.Sensor.Temp)
 		co2 = append(co2, float64(v.Sensor.CO2/10))
+		timestamp = append(timestamp, v.Timestamp)
 	}
 
-	timeRange := getRange(len(temp))
-	dimensions := 2
-	plot, _ := glot.NewPlot(dimensions, false, false)
-
-	_ = plot.SetTitle(time.Now().Format(time.Stamp))
-
-	pointGroupName := "CO2 x10"
-	style := "lines"
-	points := [][]float64{timeRange, co2}
-	_ = plot.AddPointGroup(pointGroupName, style, points)
-
-	pointGroupName = "Temp"
-	points = [][]float64{timeRange, temp}
-	_ = plot.AddPointGroup(pointGroupName, style, points)
-	_ = plot.SavePlot(config.PlotPath)
-
-	// w8 for replot
-	time.Sleep(time.Second * 2)
-}
-
-func getRange(len int) []float64 {
-	var timeRange []float64
-	for i := -1.0; i >= float64(-len); i-- {
-		timeRange = append(timeRange, i)
+	ts1 := chart.TimeSeries{
+		Style: chart.Style{
+			Show:      true,
+			FillColor: chart.ColorBlue.WithAlpha(70),
+		},
+		Name:    "Temperature",
+		XValues: timestamp,
+		YValues: temp,
 	}
-	return timeRange
+
+	ts2 := chart.TimeSeries{
+		Style: chart.Style{
+			Show:      true,
+			FillColor: chart.ColorGreen.WithAlpha(50),
+		},
+		Name:    "CO2 x10",
+		XValues: timestamp,
+		YValues: co2,
+	}
+
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			NameStyle:      chart.StyleShow(),
+			Style:          chart.StyleShow(),
+			ValueFormatter: chart.TimeValueFormatterWithFormat("15:04"),
+		},
+		YAxis: chart.YAxis{
+			NameStyle: chart.StyleShow(),
+			Style:     chart.StyleShow(),
+		},
+		Series: []chart.Series{
+			ts1,
+			ts2,
+		},
+	}
+
+	graph.Elements = []chart.Renderable{
+		chart.Legend(&graph,
+			chart.Style{FontSize: 9.0,
+				FillColor:   chart.ColorLightGray,
+				StrokeColor: chart.ColorTransparent,
+			}),
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+	err := graph.Render(chart.PNG, buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fo, err := os.Create(config.PlotPath)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := fo.Write(buffer.Bytes()); err != nil {
+		panic(err)
+	}
 }
